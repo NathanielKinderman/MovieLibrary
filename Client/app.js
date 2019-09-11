aa6056af0097a6fb58c235378a5ef3e7f3a39b6a
natek <natekinderman16@gmail.com>
2019-09-11 15:31:51 -0500

    Get functions are partially done

--- Client/app.js
+++ Client/app.js
@@ -3,7 +3,7 @@
         var dict = {
         	Title : this["title"].value,
         	Director: this["director"].value,
-          Genre: this["genre"].value
+          Genre: this ["genre"].value
         };
 
         $.ajax({
@@ -24,5 +24,104 @@
     }
 
     $('#my-form').submit( processForm );
+})(jQuery);
 
+//getall method so user can see all the movies from the list
+
+(function($){
+
+    function getAllMovies( e ){
+
+      $.ajax({
+          url: 'https://localhost:44352/api/movie',
+          dataType: 'json',
+          type: 'get',
+          contentType: 'application/json',
+          success: function( data ){
+            var json = data;
+            json.forEach(function(index, e)){
+              var row = '<tr><td>' + e.Title + '</td><td>' + e.Direcotr + '</td><td>' + e.Genre + '</td></tr>'
+              $('#all-movies tbody').append(row);
+            }            
+            // data needs to be looped through
+              $('#response pre').html( data );
+          },
+          error: function( jqXhr, textStatus, errorThrown ){
+              console.log( errorThrown );
+          }
+      });
+
+      e.preventDefault();
+    }
+    //still needs on html action link name so function can be called
+     $(#'all-movies').
+
+})(jQuery);
+
+
+
+(function($))
+{function makeTable(data)
+  {
+     $.ajax({
+      url: 'https://localhost:44352/api/movie',
+      dataType: 'json',
+      type: 'get',
+      contentType: 'application/json',
+      success: function( data ){
+
+
+          $('#response pre').html( data );
+      },
+      error: function( jqXhr, textStatus, errorThrown ){
+          console.log( errorThrown );
+      }
+  });
+
+  e.preventDefault();
+}
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+}
+
+
+
+
+
+
+  (function($){
+
+
+    function getAllMoviesById( e ){
+
+
+      $.ajax({
+          url: 'https://localhost:44352/api/movie',
+          dataType: 'json',
+          type: 'get',
+          contentType: 'application/json',
+          data: JSON.stringify(dict),
+          success: function( data, textStatus, jQxhr ){
+              $('#response pre').html( data );
+          },
+          error: function( jqXhr, textStatus, errorThrown ){
+              console.log( errorThrown );
+          }
+      });
+
+      e.preventDefault();
+    }
+    //still needs on html action link name so function can be called
+    $('#my-form').submit( getAllMovies );
 })(jQuery);
