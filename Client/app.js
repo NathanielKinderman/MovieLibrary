f5eb6258d1bec1b6a796cae265b3bdbc57b3e280
iaiir <abeyang13@gmail.com>
2019-09-11 19:28:59 -0500

    Got the get all movies api method working on html page.  Able to load all of database onto table rows.

--- Client/app.js
+++ Client/app.js
@@ -28,100 +28,19 @@
 
 //getall method so user can see all the movies from the list
 
-(function($){
-
-    function getAllMovies( e ){
-
-      $.ajax({
-          url: 'https://localhost:44352/api/movie',
-          dataType: 'json',
-          type: 'get',
-          contentType: 'application/json',
-          success: function( data ){
-            var json = data;
-            json.forEach(function(index, e)){
-              var row = '<tr><td>' + e.Title + '</td><td>' + e.Direcotr + '</td><td>' + e.Genre + '</td></tr>'
-              $('#all-movies tbody').append(row);
-            }            
-            // data needs to be looped through
-              $('#response pre').html( data );
-          },
-          error: function( jqXhr, textStatus, errorThrown ){
-              console.log( errorThrown );
-          }
-      });
-
-      e.preventDefault();
-    }
-    //still needs on html action link name so function can be called
-     $(#'all-movies').
-
-})(jQuery);
-
-
-
-(function($))
-{function makeTable(data)
-  {
-     $.ajax({
+$(document).ready(function(){
+  $.ajax({
       url: 'https://localhost:44352/api/movie',
       dataType: 'json',
       type: 'get',
       contentType: 'application/json',
-      success: function( data ){
-
-
-          $('#response pre').html( data );
-      },
-      error: function( jqXhr, textStatus, errorThrown ){
-          console.log( errorThrown );
+      data: JSON,
+      success: function ( data ){
+        var row = '';
+          $.each(data,function (index, obj){
+         row += "<tr><td>" + obj.Title + "</td><td>" + obj.Director + "</td><td>" + obj.Genre + "</td><tr>";
+          });
+          $("#all-movies").append(row);
       }
   });
-
-  e.preventDefault();
-}
-
-
-
-
-
-
-
-  }
-
-
-
-
-
-}
-
-
-
-
-
-
-  (function($){
-
-
-    function getAllMoviesById( e ){
-
-
-      $.ajax({
-          url: 'https://localhost:44352/api/movie',
-          dataType: 'json',
-          type: 'get',
-          contentType: 'application/json',
-          data: JSON.stringify(dict),
-          success: function( data, textStatus, jQxhr ){
-              $('#response pre').html( data );
-          },
-          error: function( jqXhr, textStatus, errorThrown ){
-              console.log( errorThrown );
-          }
-      });
-
-      e.preventDefault();
-    }
-    //still needs on html action link name so function can be called
-    $('#my-form').submit( getAllMovies );
-})(jQuery);
+});
