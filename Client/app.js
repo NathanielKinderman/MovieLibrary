da1f974d2f271983b81e6a44d63bd9011efb4fcd
iaiir <abeyang13@gmail.com>
2019-09-13 09:22:18 -0500

    Got the update button to work to a certain point with the edit movie function.  Just need to pass a movie id in.

--- Client/app.js
+++ Client/app.js
@@ -43,6 +43,7 @@ function getAllMovies(){
            + `<button onclick="getMovieId(${obj.MovieId})">  ${obj.Title} </button></td><td>`
            + obj.Director + "</td><td>"
            + obj.Genre + "</td><td>"
+           // + obj.MovieId + "</td><td>"//need to make movie id hidden in the table so i can grab it in my editmovie function
            + `<button onclick="populateMovie(${obj.MovieId})">Edit</button>` +
            "</td><tr>";
             });
@@ -55,6 +56,7 @@ $(document).ready(getAllMovies);
 
 //getbyid
 function getMovieId(id){
+
   $.ajax({
     url: `https://localhost:44352/api/movie/${id}/`,
     dataType: 'json',
@@ -70,21 +72,34 @@ function getMovieId(id){
 }
 
 //edit movie on table
-function editMovie(id){
+//Can get the movie to edit and update but missing the part where you are passing the id into the function.  If hardcoded Id=1 is passed in it will update object at movieid=1.
+(function($){
+function editMovie(e){
+// document.getElementById('movieId')
+  var newValue = {
+    Title : this["title"].value,
+    Director: this["director"].value,
+    Genre: this ["genre"].value
+  };
   $.ajax({
-    url: `https://localhost:44352/api/movie/${id}/`,
+    url: `https://localhost:44352/api/movie/${1}/`,//this is where the id would need to be passed in to work correctly
     dataType: 'json',
     type: 'put',
     contentType: 'application/json',
-    data: JSON.stringify(movie),
-    success: function(data){
+    data: JSON.stringify(newValue),
+    success: function(data, textStatus, jQxhr){
+      console.log("Success");
+      document.getElementById("all-movies").innerHTML = "";
+      getAllMovies();
     },
     error: function(jqXhr, textStatus, errorThrown){
       console.log("error was here")
     }
   });
+  e.preventDefault();
 }
-
+$('#my-editForm').submit( editMovie );
+})(jQuery);
 
 function populateMovie(id){
   $.ajax({
